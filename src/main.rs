312947793d3997d7a0b01b672ce9eef7edb20c55
Yudai Kotani <y1u0d2a1i@icloud.com>
2025-08-30 14:30:52 +0900

    impl test for subscription

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod;
+use std::net::TcpListener;
+use actix_web::dev::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let server = zero2prod::run(listener).expect("Failed to start server");
+    tokio::spawn(server);
+    Ok(())
 }
