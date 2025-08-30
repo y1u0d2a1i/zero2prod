b81a64e9a1f8c02e84417669405e7ccaa173a125
Yudai Kotani <y1u0d2a1i@icloud.com>
2025-08-30 16:07:25 +0900

    ensure the test pass

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,5 @@
 use std::net::TcpListener;
-
-use actix_web::test;
-use tokio::sync::mpsc::error;
+use zero2prod::startup::run;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -67,7 +65,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let port = listener.local_addr().unwrap().port();
 
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
