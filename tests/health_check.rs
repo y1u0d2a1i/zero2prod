58b5bc15e74f1fe6e9020725dd2861f5d8fd8024
Yudai Kotani <y1u0d2a1i@icloud.com>
2025-08-31 16:26:59 +0900

    add telementry to test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,35 @@
 use std::net::TcpListener;
+use tracing::subscriber;
 use uuid::Uuid;
 use zero2prod::{
-    configuration::{self, DatabaseSettings},
+    configuration::{DatabaseSettings, get_configration},
     startup::run,
+    telemetry::{get_subscriber, init_subscriber},
 };
 
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use zero2prod::configuration::get_configration;
+use std::sync::LazyLock;
+
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink
+        );
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -82,6 +105,8 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 async fn spawn_app() -> TestApp {
+    LazyLock::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
